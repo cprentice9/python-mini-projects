3b883c4ffe9fa1e83226bf97eec55fb6625d7ee5
Connor Prentice <connurp@gmail.com>
2023-03-28 14:57:28 -0500

    started adventure game

---
+++ choose_your_adventure.py
@@ -0,0 +1,6 @@
+name = input("Type your name: ")
+print("Welcome", name, "to this adventure!")
+
+answer = input(
+    "You are on a dirt road and it has come to an end. Would you like to go left or right?"
+)
