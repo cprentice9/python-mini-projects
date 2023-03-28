e4f38b04562f9966b51f2bbfb85ad4d655e010b1
Connor Prentice <connurp@gmail.com>
2023-03-27 21:58:33 -0500

    finished up rock paper scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,9 +2,9 @@ import random
 
 user_wins = 0
 computer_wins = 0
+draw = 0
 
 options = ["rock", "paper", "scissors"]
-options[1, 2, 3]
 
 while True:
     user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
@@ -14,10 +14,10 @@ while True:
     if user_input not in options:
         continue
 
-    random_number = random.randint(1, 3)
-    # rock: 1
-    # paper: 2
-    # scissors: 3
+    random_number = random.randint(0, 2)
+    # rock: 0
+    # paper: 1
+    # scissors: 2
     computer_pick = options[random_number]
     print("Computer picked", computer_pick + ".")
 
@@ -25,4 +25,23 @@ while True:
         print("You win!")
         user_wins += 1
 
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You win!")
+        user_wins += 1
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You win!")
+        user_wins += 1
+
+    elif user_input == "rock" and computer_pick == "rock":
+        print("It's a tie!")
+        draw += 1
+
+    else:
+        print("You lose!")
+        computer_wins += 1
+
+print("You won", user_wins, "times.")
+print("Computer won", computer_wins, "times.")
+print("You and the computer tied", draw, "times.")
 print("Goodbye!")
