1318ca92a1f369c587ed65d7bfc0332ac055f8b0
Connor Prentice <connurp@gmail.com>
2024-01-26 20:09:56 -0600

    teaching my kid Python

--- practice.py
+++ practice.py
@@ -1,4 +1,69 @@
-name = input('Name: ')
-age = input('Age: ')
+# name = input("Name: ")
+# age = input("Age: ")
 
-print('Hello', name, 'you are', age, 'years old.')
+# print("Hello", name, "you are", age, "years old.")
+
+# x = 9
+# y = 3
+
+# result = x + y + 100
+# print(result)
+
+# num = input("Number: ")
+# print(int(num) - 5)
+
+# hello = "HeLLo World"
+# print(hello.lower().count("ll"))
+
+# x = "hello"
+# y = "hello"
+
+# print(x == y)
+
+# x = 7
+# y = 8
+# z = 0
+
+# result1 = x == y
+# result2 = y > x
+# result3 = z < x + 2
+
+# result4 = result1 or not result2 or result3
+# print(result4)
+
+# x = input("Name: ")
+
+# if x == "Connor":
+#     print("Hello Connor")
+# elif x == "Lincoln":
+#     print("Welcome Lincoln")
+# elif x == "Zandra":
+#     print("Welcome Zandra")
+# else:
+#     print("You don't belong here " + x)
+
+# x = [4, True, "hi"]
+# x.extend([5, False, "hello"])
+# print(x.pop())
+# print(x)
+
+# x = (0, 0, 2, 2)
+# x.append(3)
+# print(x[0])
+
+# for i in range(1, 12, 2):
+#     print(i)
+
+# x = [1, 2, 3, 4, 5, 6]
+
+# for i, element in enumerate(x):
+#     print(i, element)
+
+# i = 7
+# while i < 10:
+#     print("run")
+#     i += 1
+
+x = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+y = ["hi", "hello", "goodbye", "cya", "sure"]
+s = "hello"
